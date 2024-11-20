80fa2e678c75cc226c968742f6bcbbcc862118cb
GenaPlem <shainkill1337@gmail.com>
2024-11-20 14:45:42 +0000

    Add Dummy posts data and update Home page with it

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,38 @@
+import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from "../components/home-page/hero";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React framework for production - it makes building fullstack React app easier and faster.",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React framework for production - it makes building fullstack React app easier and faster.",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React framework for production - it makes building fullstack React app easier and faster.",
+    date: "2022-02-10",
+  },
+];
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </>
   );
 }
