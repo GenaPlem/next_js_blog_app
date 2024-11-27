f27a7be8eb466eeb549f268ecf86c9424423f031
GenaPlem <shainkill1337@gmail.com>
2024-11-27 14:45:12 +0000

    Add notification component and styles

---
+++ components/ui/notification.js
@@ -0,0 +1,24 @@
+import styles from "./notification.module.css";
+
+export default function Notification(props) {
+  const { title, message, status } = props;
+
+  let statusClasses = "";
+
+  if (status === "success") {
+    statusClasses = styles.success;
+  }
+
+  if (status === "error") {
+    statusClasses = styles.error;
+  }
+
+  const cssClasses = `${styles.notification} ${statusClasses}`;
+
+  return (
+    <div className={cssClasses}>
+      <h2>{title}</h2>
+      <p>{message}</p>
+    </div>
+  );
+}
