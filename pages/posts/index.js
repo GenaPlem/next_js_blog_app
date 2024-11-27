e0f7010a99e76f490b42c60f9947ae32cf16c319
GenaPlem <shainkill1337@gmail.com>
2024-11-27 12:46:28 +0000

    Update All posts page with data from md file

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,32 +1,16 @@
 import AllPosts from "../../components/posts/all-posts";
+import { getAllPosts } from "../../lib/posts-util";
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React app easier and faster.",
-    date: "2022-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React app easier and faster.",
-    date: "2022-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React app easier and faster.",
-    date: "2022-02-10",
-  },
-];
+export default function AllPostsPage({ posts }) {
+  return <AllPosts posts={posts} />;
+}
 
-export default function AllPostsPage() {
-  return <AllPosts posts={DUMMY_POSTS} />;
+export function getStaticProps() {
+  const allPosts = getAllPosts();
+
+  return {
+    props: {
+      posts: allPosts,
+    },
+  };
 }
