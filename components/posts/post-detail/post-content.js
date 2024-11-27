f0bebcbb37a250289c2f5906f71580bd4bc91752
GenaPlem <shainkill1337@gmail.com>
2024-11-27 13:42:51 +0000

    Update appearance of code snippets from md files

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,5 +1,7 @@
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
+import { Prism as SyntaxHighLighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import PostHeader from "./post-header";
 import styles from "./post-content.module.css";
@@ -35,6 +37,19 @@ export default function PostContent({ post }) {
           </div>
         );
       }
+      return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1];
+      return (
+        <SyntaxHighLighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
     },
   };
 
